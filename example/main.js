04381023d5baf4e6444777ed8966f5cd954bfef8
matt <matt@beast.(none)>
2013-09-17 21:17:31 -0700

    re written to be extensible

--- example/main.js
+++ example/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-  $('#remove').removeable();
+  $('#remove').swipe('swipe', function () {
+    //call this function upon completion of the gesture
+    $('#remove').remove();
+  }, function () {
+    //call this function upon initiation of the gesture
+  }, {
+    //options
+  });
 });
